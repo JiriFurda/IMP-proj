9a4a9ebf921beadd5bc636cd13574004b9fe28c0
Furdys <magiecz@gmail.com>
2018-12-30 22:55:50 +0100

    Commentaries, help and project description

--- mcu/main.c
+++ mcu/main.c
@@ -1,3 +1,9 @@
+/*
+ * Author: Jiri Furda (xfurda00)
+ * Changes: 90% (whole Tetris and LED matrix logic)
+ * Last change: 2018-12-30
+ */
+
 /*******************************************************************************
    main.c: LCD + keyboard demo
    Copyright (C) 2009 Brno University of Technology,
@@ -48,10 +54,9 @@
 #include <lcd/display.h>
 #include <stdlib.h>
 
-char last_ch; //naposledy precteny znak
-char char_cnt;
+char last_ch; // naposledy precteny znak
 
-int field[9][10] =
+int field[9][10] =  // Static playing field (including boundaries, excluding current shape)
         {
                 {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
                 {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
@@ -64,7 +69,7 @@ int field[9][10] =
                 {1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
         };
 
-int led_matrix[8][8] =
+int led_matrix[8][8] =  // Playing field shown on led matrix (including current shape)
         {
                 {0, 0, 0, 0, 0, 0, 0, 0},
                 {0, 0, 0, 0, 0, 0, 0, 0},
@@ -76,45 +81,46 @@ int led_matrix[8][8] =
                 {0, 0, 0, 0, 0, 0, 0, 0}
         };
 
-//int shape;
-int rotation;
-int sRow;
-int sCol;
-int sCurr;
-int sNext;
+int sRot;   // Rotation of current shape
+int sRow;   // y position of current shape
+int sCol;   // x position fo current shape
+int sCurr;  // Type of current shape
+int sNext;  // Type of next shape
 
-int gameOver = 0;
+int gameOver = 0;   // Value indicating end of the game
 
-int lettersCount = 7;
-char letters[] = "IJLOSTZ";
-int shapes[][4][4][4] =
+int lettersCount = 7;   // Total count of shape types
+char letters[] = "IJLOSTZ"; // Letter representation of shape types (order must correspond with shapes array)
+int shapes[][4][4][4] = // Shape types with all their rotations
         {
+            // Shape I
             {
-                {
-                    {0, 0, 0, 0},
-                    {1, 1, 1, 1},
-                    {0, 0, 0, 0},
-                    {0, 0, 0, 0}
-                },
-                {
-                    {0, 0, 1, 0},
-                    {0, 0, 1, 0},
-                    {0, 0, 1, 0},
-                    {0, 0, 1, 0}
-                },
-                {
-                    {0, 0, 0, 0},
-                    {0, 0, 0, 0},
-                    {1, 1, 1, 1},
-                    {0, 0, 0, 0}
-                },
-                {
-                    {0, 1, 0, 0},
-                    {0, 1, 0, 0},
-                    {0, 1, 0, 0},
-                    {0, 1, 0, 0}
-                }
+                    {
+                            {0, 0, 0, 0},
+                            {1, 1, 1, 1},
+                            {0, 0, 0, 0},
+                            {0, 0, 0, 0}
+                    },
+                    {
+                            {0, 0, 1, 0},
+                            {0, 0, 1, 0},
+                            {0, 0, 1, 0},
+                            {0, 0, 1, 0}
+                    },
+                    {
+                            {0, 0, 0, 0},
+                            {0, 0, 0, 0},
+                            {1, 1, 1, 1},
+                            {0, 0, 0, 0}
+                    },
+                    {
+                            {0, 1, 0, 0},
+                            {0, 1, 0, 0},
+                            {0, 1, 0, 0},
+                            {0, 1, 0, 0}
+                    }
             },
+            // Shape J
             {
                     {
                             {1, 0, 0, 0},
@@ -141,6 +147,7 @@ int shapes[][4][4][4] =
                             {0, 0, 0, 0}
                     }
             },
+            // Shape L
             {
                     {
                             {0, 0, 1, 0},
@@ -167,6 +174,7 @@ int shapes[][4][4][4] =
                             {0, 0, 0, 0}
                     }
             },
+            // Shape O
             {
                     {
                             {0, 1, 1, 0},
@@ -193,6 +201,7 @@ int shapes[][4][4][4] =
                             {0, 0, 0, 0}
                     }
             },
+            // Shape S
             {
                     {
                             {0, 1, 1, 0},
@@ -219,6 +228,7 @@ int shapes[][4][4][4] =
                             {0, 0, 0, 0}
                     }
             },
+            // Shape T
             {
                     {
                             {0, 1, 0, 0},
@@ -245,6 +255,7 @@ int shapes[][4][4][4] =
                             {0, 0, 0, 0}
                     }
             },
+            // Shape Z
             {
                     {
                             {1, 1, 0, 0},
@@ -273,26 +284,38 @@ int shapes[][4][4][4] =
             }
         };
 
+
+/**
+ * Checks whether position is occupied or not
+ * @param row Y position to check
+ * @param col X position to check
+ * @return 1 if occupied, 0 if not
+ */
 int is_occupied(int row, int col)
 {
+    // Check if position is occupied by static field
     if(field[row][col] == 1)
         return 1;
 
-
+    // Check if position is occupied by current shape
     if(row >= sRow && row < sRow+4 && col >= sCol && col < sCol+4)
-        return shapes[sCurr][rotation][row-sRow][col-sCol];
+        return shapes[sCurr][sRot][row-sRow][col-sCol];
 
+    // Position is not occupied
     return 0;
 }
 
-void field_print(void)
+
+/**
+ * Merges visible part of static field and current shape into visible 8x8 field and prints it to terminal
+ * Must be called every time the field has changed
+ */
+void field_changed(void)
 {
     term_send_crlf();
 
     int row;
     int col;
-
-
 
     for(row = 0; row < 8; row++)
     {
@@ -314,15 +337,23 @@ void field_print(void)
     }
 }
 
+
+/**
+ * Determine next shape to be spawn and prints it to LCD
+ */
 void generate_next_shape(void)
 {
-    sNext = (sCurr+1) % lettersCount; // @todo rand() is not working
+    sNext = (sCurr+1) % lettersCount;
 
     LCD_clear();
     LCD_append_string("Next shape: ");
     LCD_append_char(letters[sNext]);
 }
 
+
+/**
+ * Ends the game and prints "GAME OVER!" to LCD
+ */
 void game_over(void)
 {
     LCD_clear();
@@ -330,16 +361,21 @@ void game_over(void)
     gameOver = 1;
 }
 
+
+/**
+ * Spawns next shape or end the game if there is no space to spawn
+ */
 void shape_spawn(void)
 {
+    // Default values for new current shape
     int newRotation = 0;
     int newRow = 0;
     int newCol = 3;
     int newShape = sNext;
 
+    // Checks for collision
     int row;
     int col;
-
     for(row = 0; row < 4; row++)
     {
         for(col = 0; col < 4; col++)
@@ -349,16 +385,19 @@ void shape_spawn(void)
         }
     }
 
-
-    rotation = newRotation;
+    // Setup current shape
+    sRot = newRotation;
     sRow = newRow;
     sCol = newCol;
-
     sCurr = newShape;
+
     generate_next_shape();
 }
 
 
+/**
+ * Rotates the current shape if there is enough space
+ */
 void shape_rotate(void)
 {
     int attempts;
@@ -367,11 +406,13 @@ void shape_rotate(void)
     int newRotation;
     int failed = 1; // 1 to pass the first iteration
 
+    // Check if any rotation can be done
     for(attempts = 1; attempts < 4 && failed; attempts++)
     {
         failed = 0;
-        newRotation = (rotation+attempts) % 4;
+        newRotation = (sRot+attempts) % 4;
 
+        // Check for collision
         for(row = 0; row < 4 && !failed; row++)
         {
             for(col = 0; col < 4 && !failed; col++)
@@ -383,11 +424,18 @@ void shape_rotate(void)
         }
     }
 
+    // Revert rotation if cannot be done
     if(!failed)
-        rotation = newRotation;
+        sRot = newRotation;
 }
 
 
+/**
+ * Moves the current shape if there is no collision
+ * @param rowChange Increment of Y position
+ * @param colChange Increment of X position
+ * @return 1 if move was successful, 0 if failed
+ */
 int shape_move(int rowChange, int colChange)
 {
     int row;
@@ -397,7 +445,7 @@ int shape_move(int rowChange, int colChange)
     {
         for(col = 0; col < 4; col++)
         {
-            if(shapes[sCurr][rotation][row][col] == 1)
+            if(shapes[sCurr][sRot][row][col] == 1)
                 if(field[sRow+row+rowChange][sCol+col+colChange])
                     return 0;
         }
@@ -409,6 +457,10 @@ int shape_move(int rowChange, int colChange)
     return 1;
 }
 
+
+/**
+ * Adds current shape to static field
+ */
 void shape_static()
 {
     int row;
@@ -418,12 +470,17 @@ void shape_static()
     {
         for(col = 0; col < 4; col++)
         {
-            if(shapes[sCurr][rotation][row][col] == 1)
+            if(shapes[sCurr][sRot][row][col] == 1)
                 field[sRow+row][sCol+col] = 1;
         }
     }
 }
 
+
+/**
+ * Clears whole row except boundaries
+ * @param row Y position
+ */
 void row_clear(int row)
 {
     int col;
@@ -434,6 +491,12 @@ void row_clear(int row)
     }
 }
 
+
+/**
+ * Checks whether row is full
+ * @param row Y position
+ * @return 1 if row is full, 0 if not
+ */
 int row_is_full(int row)
 {
     int col;
@@ -445,7 +508,13 @@ int row_is_full(int row)
     return 1;
 }
 
-int row_copy(int destRow, int srcRow)
+
+/**
+ * Copies one row to other row
+ * @param destRow destination Y position
+ * @param srcRow source Y position
+ */
+void row_copy(int destRow, int srcRow)
 {
     int col;
     for(col = 1; col < 9; col++)
@@ -454,6 +523,11 @@ int row_copy(int destRow, int srcRow)
     }
 }
 
+
+/**
+ * Moves all the rows starting from parameter "row"+1 to 0 by one row lower
+ * @param row starting Y position
+ */
 void field_drop(int row)
 {
     for(; row >= 0; row--)
@@ -468,7 +542,11 @@ void field_drop(int row)
     }
 }
 
-void field_clear()
+
+/**
+ * Check all the rows (except bottom boundary) whether are full and drops the field above them
+ */
+void field_clear_full_rows()
 {
     int row;
 
@@ -482,48 +560,61 @@ void field_clear()
     }
 }
 
+
+/**
+ * Moves current shape one row lower or sets new current shape and clears full rows in field
+ */
 void shape_down()
 {
     if(!shape_move(1, 0))
     {
         shape_static();
-        field_clear();
+        field_clear_full_rows();
         shape_spawn();
     }
 }
 
-void led_light_col(int row) // light columns in the row
+
+/**
+ * Lights columns in the LED matrix for one specified row
+ * @param led_row currently lit row
+ */
+void led_light_col(int led_row)
 {
     P3DIR &= ~(BIT2);
     P2DIR = 0;
 
-    if(led_matrix[row][0])
+    if(led_matrix[led_row][0])
         P2DIR |= BIT0;
 
-    if(led_matrix[row][1])
+    if(led_matrix[led_row][1])
         P2DIR |= BIT1;
 
-    if(led_matrix[row][2])
+    if(led_matrix[led_row][2])
         P3DIR |= BIT2;
 
-    if(led_matrix[row][3])
+    if(led_matrix[led_row][3])
         P2DIR |= BIT3;
 
-    if(led_matrix[row][4])
+    if(led_matrix[led_row][4])
         P2DIR |= BIT4;
 
-    if(led_matrix[row][5])
+    if(led_matrix[led_row][5])
         P2DIR |= BIT5;
 
-    if(led_matrix[row][6])
+    if(led_matrix[led_row][6])
         P2DIR |= BIT6;
 
-    if(led_matrix[row][7])
+    if(led_matrix[led_row][7])
         P2DIR |= BIT7;
 }
 
-int led_row = 0;
-void led_light_row()
+
+/**
+ * Lights one specified row in the LED matrix
+ * @param led_row row to be lit
+ */
+void led_light_row(int led_row)
 {   // lights whole row
     switch(led_row)
     {
@@ -554,18 +645,6 @@ void led_light_row()
     }
 
     led_light_col(led_row);
-
-    led_row = (led_row + 1) % 8;
-
-
-
-    /*
-    led_show_row(7-led_col);
-
-    P6OUT = ~(1 << (7-led_col));
-
-    led_col = (led_col + 1) % 8;
-    */
 }
 
 
@@ -576,6 +655,18 @@ void led_light_row()
 *******************************************************************************/
 void print_user_help(void)
 {
+    term_send_crlf();
+    term_send_str("Tetris on 8x8 matrix by xfurda00");
+    term_send_crlf();
+    term_send_str("Controls:");
+    term_send_crlf();
+    term_send_str("key 4 to move left");
+    term_send_crlf();
+    term_send_str("key 6 to move right");
+    term_send_crlf();
+    term_send_str("key 5 to move down");
+    term_send_crlf();
+    term_send_str("key 2 to rotate");
 }
 
 
@@ -613,7 +704,7 @@ int keyboard_idle()
                 break;
         }
 
-        field_print();
+        field_changed();
     }
     return 0;
 }
@@ -644,28 +735,25 @@ void fpga_initialized()
 *******************************************************************************/
 int main(void)
 {
-    unsigned int cnt = 0;
-    char_cnt = 0;
-    last_ch = 0;
-
+    // Setup hardware
     initialize_hardware();
-    WDG_stop();
     keyboard_init();
-
-    set_led_d6(1);                       // rozsviceni D6
-    set_led_d5(1);                       // rozsviceni D5
-
-    generate_next_shape();
-    shape_spawn();
-    field_print();
-
-    TACTL = TASSEL_1 + MC_2;
     P6DIR = 255; // 11111111
 
+    // Setup software
+    last_ch = 0;
+    generate_next_shape();
+    shape_spawn();
+    field_changed();
+
+    // Main cycle
+    int led_row = 0;
     while (1)
     {
-        delay_ms(1);
-        led_light_row();
+        delay_ms(1);    // Wait
+
+        led_light_row(led_row); // Light up one row
+        led_row = (led_row + 1) % 8;    // Determine next row to be lit
 
         keyboard_idle();                   // obsluha klavesnice
         terminal_idle();                   // obsluha terminalu
